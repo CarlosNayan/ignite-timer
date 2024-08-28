08394596ca5b16d8ff7e470c8c65a6248826f4c4
Carlos Nayan <carlosnayan@gmail.com>
2024-08-28 08:47:31 -0300

    feat: finished history page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,6 +23,7 @@ const HeaderContainer = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  padding: 0 2rem;
 
   nav {
     display: flex;
--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,21 +1,129 @@
+import styled from "styled-components";
+
 export function HistoryPage() {
-	return (
-	  <div style={{ flex: 1, alignItems: "center", padding: 24 }}>
-		<div
-		  style={{
-			flex: 1,
-			justifyContent: "center",
-			maxWidth: 960,
-			marginInline: "auto",
-			flexDirection: "column",
-		  }}
-		>
-		  <text style={{ fontSize: 64, fontWeight: "bold" }}>History page</text>
-		  <text style={{ fontSize: 36, color: "#38434D" }}>
-			Essa é a página de histórico
-		  </text>
-		</div>
-	  </div>
-	);
+  return (
+    <HistoryContainer>
+      <h1>Meu Histórico</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status variant="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status variant="red">Interrompido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status variant="yellow">Em andamento</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
+}
+
+const HistoryContainer = styled.main`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 3.5rem;
+
+  padding: 2rem;
+
+  h1 {
+    font-size: 1.5rem;
+    color: ${({ theme }) => theme["gray-100"]};
   }
-  
+`;
+
+const HistoryList = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+
+  overflow: auto;
+  margin-top: 2rem;
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    min-width: 600px;
+
+    th {
+      background-color: ${({ theme }) => theme["gray-600"]};
+      padding: 1rem;
+      text-align: left;
+      color: ${({ theme }) => theme["gray-100"]};
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        border-top-left-radius: 8px;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        border-top-right-radius: 8px;
+        padding-right: 1.5rem;
+      }
+    }
+
+    td {
+      background-color: ${({ theme }) => theme["gray-700"]};
+      border-top: 4px solid ${({ theme }) => theme["gray-800"]};
+      padding: 1rem;
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        width: 50%;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        padding-right: 1.5rem;
+      }
+    }
+  }
+`;
+
+const Status = styled.span<{ variant: "yellow" | "green" | "red" }>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: "";
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 9999px;
+
+    background-color: ${({ theme, variant }) => theme[`${variant}-500`]};
+  }
+`;
